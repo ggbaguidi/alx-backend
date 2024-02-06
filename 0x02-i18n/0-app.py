427d5035e6cf7d47cf7d92c1a693fe0077e0503c
Guy Ahonakpon GBAGUIDI <guygbaguidi123root@gmail.com>
2024-02-07 00:11:41 +0100

    feat: add project files

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -6,7 +6,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route('/', strict_slashes=False)
 def get_basic_flask_app() -> str:
     """Basic Flask app function"""
 
@@ -14,4 +14,4 @@ def get_basic_flask_app() -> str:
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=5000, debug=True)
